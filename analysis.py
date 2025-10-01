292695d8d25193d8434b0a1a7bbeaecd43dcbcc1
Nikola <stankovicnikola01@gmail.com>
2025-10-01 12:25:56 +0200

    Improved and fixed analysis.py. Extracted all constants to constants.py.

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc
+import constants
 
 
 def load_processed_data(path):
@@ -14,7 +15,7 @@ def load_processed_data(path):
     return d1
 
 
-def plot_confusion_matrix(y_true_dict, y_pred_dict, x_label="Predicted", y_label="Actual", title="Confusion matrix"):
+def plot_confusion_matrix(y_true_dict, y_pred_dict, x_label="Predicted", y_label="Actual", title="Confusion matrix", save_path=None):
     if y_true_dict.keys() != y_pred_dict.keys():
         raise ValueError()
 
@@ -31,7 +32,9 @@ def plot_confusion_matrix(y_true_dict, y_pred_dict, x_label="Predicted", y_label
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
-    plt.show()
+
+    if save_path:
+        plt.savefig(save_path)
 
     # Values have to be ints for the metric calculation, so they are converted.
     y_true_ints = [1 if val == "True" else 0 for val in y_true]
@@ -50,7 +53,7 @@ def plot_confusion_matrix(y_true_dict, y_pred_dict, x_label="Predicted", y_label
     }
 
 
-def plot_roc_curve(y_true_dict, y_pred_dict, title="ROC curve"):
+def plot_roc_curve(y_true_dict, y_pred_dict, title="ROC curve", save_path=None):
     if y_true_dict.keys() != y_pred_dict.keys():
         raise ValueError()
 
@@ -66,30 +69,57 @@ def plot_roc_curve(y_true_dict, y_pred_dict, title="ROC curve"):
     roc_auc = auc(fpr, tpr)
 
     plt.figure(figsize=(6, 6))
-    plt.plot(fpr, tpr, color="blue", lw=2, label=title)
+    plt.plot(fpr, tpr, color="blue", lw=2, label="ROC curve")
     plt.plot([0, 1], [0, 1], color="gray", lw=2, linestyle="--", label="Random guess")
     plt.xlabel("False Positive rate")
     plt.ylabel("True Positive rate")
     plt.title(title)
     plt.legend(loc="lower right")
     plt.grid(True, linestyle="--", alpha=0.7)
-    plt.show()
+
+    if save_path:
+        plt.savefig(save_path)
 
     return roc_auc
 
 
 if __name__ == "__main__":
-    data1_csv_path = "./results/spagft_results_mouse_embryo.csv"
-    data2_csv_path = "./results/fabricated_results_temp.csv"
+    display_names = constants.display_names
 
-    data1 = load_processed_data(data1_csv_path)
-    data2 = load_processed_data(data2_csv_path)
-    results = plot_confusion_matrix(data1, data2)
+    with open(f"{constants.result_directory}/output.txt", "w") as f:
+        for subject_algorithm in constants.subject_algorithms:
+            for dataset_name in constants.dataset_names:
+                reference_csv_path = f"{constants.result_directory}/{constants.reference_algorithm}_results_{dataset_name}.csv"
+                subject_csv_path = f"{constants.result_directory}/{subject_algorithm}_results_{dataset_name}.csv"
 
-    print(f"Accuracy:  {results['accuracy']:.4f}")
-    print(f"Precision: {results['precision']:.4f}")
-    print(f"Recall:    {results['recall']:.4f}")
-    print(f"F1 Score:  {results['f1_score']:.4f}")
+                data1 = load_processed_data(reference_csv_path)
+                data2 = load_processed_data(subject_csv_path)
 
-    auc = plot_roc_curve(data1, data2)
-    print(f"Area under ROC curve: {auc:.2f}")
+                f.write(f"{display_names.get(constants.reference_algorithm)} vs {display_names.get(subject_algorithm)} on {display_names.get(dataset_name)} dataset:\n")
+
+                cm_save_path = f"{constants.plots_directory}/cm_{subject_algorithm}_{dataset_name}.png"
+                cm_title = f"Confusion matrix - {display_names.get(subject_algorithm)} ({display_names.get(dataset_name)})"
+
+                try:
+                    results = plot_confusion_matrix(data1, data2, title=cm_title, save_path=cm_save_path)
+                except ValueError:
+                    print("Error while plotting confusion matrix.")
+
+                f.write(f"Accuracy:  {results['accuracy']:.4f}\n")
+                f.write(f"Precision: {results['precision']:.4f}\n")
+                f.write(f"Recall:    {results['recall']:.4f}\n")
+                f.write(f"F1 Score:  {results['f1_score']:.4f}\n")
+
+                roc_save_path = f"{constants.plots_directory}/roc_{subject_algorithm}_{dataset_name}.png"
+                roc_title = f"ROC curve - {display_names.get(subject_algorithm)} ({display_names.get(dataset_name)})"
+
+                try:
+                    roc_area = plot_roc_curve(data1, data2, title=roc_title, save_path=roc_save_path)
+                except ValueError:
+                    print("Error while plotting ROC curve.")
+
+                f.write(f"Area under ROC curve: {roc_area:.2f}\n")
+                f.write("\n")
+
+        print(f"Plots were saved to {constants.plots_directory}!")
+        print(f"Results were saved to {constants.result_directory}/output.txt!")
--- constants.py
+++ constants.py
@@ -1,2 +1,15 @@
 MOUSE_EMBRYO_DATA_PATH = "./assets/E9.5_E1S1.MOSTA.h5ad"
 MOUSE_BRAIN_DATA_PATH = "./assets/Mouse_brain_cell_bin.h5ad"
+
+result_directory = "./results"
+plots_directory = "./plots"
+dataset_names = ["mouse_embryo", "mouse_brain"]
+reference_algorithm = "spagft"
+subject_algorithms = ["shannon_entropy", "spatial_knn", "anova"]
+
+display_names = {"mouse_embryo": "Mouse embryo",
+                 "mouse_brain": "Mouse brain",
+                 "spagft": "SpaGFT",
+                 "shannon_entropy": "Shannon entropy",
+                 "spatial_knn": "Spatial KNN",
+                 "anova": "Anova"}
